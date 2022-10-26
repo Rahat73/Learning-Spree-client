96c143ca86ec34754671fda6c170000dcfdef1e7
Rahat73 <55106915+Rahat73@users.noreply.github.com>
2022-10-26 14:52:28 +0600

    footer added

--- src/Components/Courses/Courses.js
+++ src/Components/Courses/Courses.js
@@ -10,8 +10,8 @@ const Courses = () => {
     return (
         <div>
             <div className='w-11/12 mx-auto grid grid-cols-2 md:grid-cols-4 gap-7'>
-                <div className='sm:col-span-2 md:col-span-1'><Sidebar></Sidebar></div>
-                <div className='sm:col-span-2 md:col-span-3'>
+                <div className='col-span-2 md:col-span-1'><Sidebar></Sidebar></div>
+                <div className='col-span-2 md:col-span-3'>
                     <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
                         {
                             allCourses.map(course => <CourseCard key={course.id} course={course}></CourseCard>)
