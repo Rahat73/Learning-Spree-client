36e9f405e3e2164a10fd71c9364fdf6cf804f31b
Rahat73 <55106915+Rahat73@users.noreply.github.com>
2022-10-26 21:02:51 +0600

    login registration updated

--- src/Components/LoginRegistration/Login/Login.js
+++ src/Components/LoginRegistration/Login/Login.js
@@ -2,8 +2,25 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-            this is login
+        <div className='w-5/12 mx-auto mb-20'>
+            <form>
+                <div className='bg-slate-300 bg-opacity-10 bg-clip-padding backdrop-filter backdrop-blur-sm rounded-2xl p-10'>
+                    <div className="divider text-2xl">Login</div>
+                    <div className='form-control w-1/2 mx-auto'>
+                        <label className="label">
+                            <span className="label-text text-xl">Email</span>
+                        </label>
+                        <input type="email" name='email' placeholder="Enter your email" className="input input-bordered w-full max-w-xs" required />
+                        <label className="label">
+                            <span className="label-text text-xl">Password</span>
+                        </label>
+                        <input type="password" name='password' placeholder="Enter your password" className="input input-bordered w-full max-w-xs" required />
+                        <div>
+                            <button className='btn btn-wide btn-outline btn-secondary mt-10 mb-5' type='submit'>Login</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
         </div>
     );
 };
